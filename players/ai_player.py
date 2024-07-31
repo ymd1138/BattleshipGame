37131a9ee0f6d1ea5798a6e8564b53ef62fff1e7
ymd1138 <74826010+ymd1138@users.noreply.github.com>
2024-07-31 22:16:45 +0900

    修正: 周囲のマスの数に基づいて加算を行う
    周囲のマスを計算するメソッドを追加し、nearとhitに含まれない場合の処理を実装．
    
    2で割るメソッドを追加．
    
    正規化するメソッドを追加．

--- players/ai_player.py
+++ players/ai_player.py
@@ -109,7 +109,6 @@ class AIPlayer(Player):
             elif ship == 's':
                 self.pred_s = self.move_predictions(self.pred_s, dx, dy)
 
-
         # resultとattackedが存在すれば下に進む
         if 'result' in data and 'attacked' in data['result']:
             # 自分もしくは相手の攻撃結果を受け取る
@@ -118,69 +117,58 @@ class AIPlayer(Player):
             hit = result.get('hit')
             near = result.get('near', [])
             
-            # 自分のターンの終わりなら、自分の攻撃結果を基にスコアを更新する．
+            # 自分のターンの終わりなら，自分の攻撃結果を基にスコアを更新する．
             if is_my_turn:
-                # hitした場合は、hitした場所を1にして、それ以外を0にする．
+                # hitした場合は，hitした場所を1にして，それ以外を0にする．
                 if hit == 'w':
                     self.pred_w = [[0] * Player.FIELD_SIZE for _ in range(Player.FIELD_SIZE)]
                     self.pred_w[position[0]][position[1]] = 1
                 elif hit == 'c':
                     self.pred_c = [[0] * Player.FIELD_SIZE for _ in range(Player.FIELD_SIZE)]
                     self.pred_c[position[0]][position[1]] = 1
-                # sに攻撃が命中した場合は撃沈するので，上で初期化すればよい．
+                # sに攻撃が命中した場合は撃沈するので，上で初期化する．
 
-                # hitしなかったら、そのマスは0にする．
+                # hitしなかったら，そのマスは0にする．
                 else:
                     self.pred_w[position[0]][position[1]] = 0
                     self.pred_c[position[0]][position[1]] = 0
                     self.pred_s[position[0]][position[1]] = 0
+                    # 正規化を行い，合計が1になるように値を修正する．
+                    self.normalize(self.pred_w)
+                    self.normalize(self.pred_c)
+                    self.normalize(self.pred_s)
                     
-                # nearの場合、周囲1マスに足して、中心は0にする
+                # nearの場合，周囲1マスに足して，中心は0にする．
                 for n in near:
                     if n == 'w':
-                        for dx in [-1, 0, 1]:
-                            for dy in [-1, 0, 1]:
-                                x, y = position[0] + dx, position[1] + dy
-                                if Player.in_field([x, y]):
-                                    self.pred_w[x][y] += 0.125
-                        self.pred_w[position[0]][position[1]] = 0
-                        self.pred_w = [[n / 2 for n in sublist] for sublist in self.pred_w]
+                        self.update_near_predictions(self.pred_w, position)
                     elif n == 'c':
-                        for dx in [-1, 0, 1]:
-                            for dy in [-1, 0, 1]:
-                                x, y = position[0] + dx, position[1] + dy
-                                if Player.in_field([x, y]):
-                                    self.pred_c[x][y] += 0.125
-                        self.pred_c[position[0]][position[1]] = 0
-                        self.pred_c = [[n / 2 for n in sublist] for sublist in self.pred_c]
+                        self.update_near_predictions(self.pred_c, position)
                     elif n == 's':
-                        for dx in [-1, 0, 1]:
-                            for dy in [-1, 0, 1]:
-                                x, y = position[0] + dx, position[1] + dy
-                                if Player.in_field([x, y]):
-                                    self.pred_s[x][y] += 0.125
-                        self.pred_s[position[0]][position[1]] = 0
-                        self.pred_s = [[n / 2 for n in sublist] for sublist in self.pred_s]
+                        self.update_near_predictions(self.pred_s, position)
 
+                # nearとhitに含まれない場合，周囲のマスを0にする
+                if 'w' not in near and hit != 'w':
+                    self.clear_around_predictions(self.pred_w, position)
+                    self.normalize(self.pred_w)
+                if 'c' not in near and hit != 'c':
+                    self.clear_around_predictions(self.pred_c, position)
+                    self.normalize(self.pred_c)
+                if 's' not in near and hit != 's':
+                    self.clear_around_predictions(self.pred_s, position)
+                    self.normalize(self.pred_s)
+                    
             # 相手のターンの終わりなら，相手の攻撃結果を基にスコアを更新する．
-            # すべての艦の予測マップについて、相手が攻撃した場所の周囲1マス（中心も含む）に足す．
+            # すべての艦の予測マップについて，相手が攻撃した場所の周囲1マス（中心も含む）に足す．
             else:
                 for ship_type in ['w', 'c', 's']:
                     if self.previous_enemy_ships[ship_type]:
-                        for dx in [-1, 0, 1]:
-                            for dy in [-1, 0, 1]:
-                                x, y = position[0] + dx, position[1] + dy
-                                if Player.in_field([x, y]):
-                                    if ship_type == 'w':                                 
-                                        self.pred_w[x][y] += 1/9
-                                    elif ship_type == 'c':
-                                        self.pred_c[x][y] += 1/9
-                                    elif ship_type == 's':
-                                        self.pred_s[x][y] += 1/9
-                                    
+                        self.update_around_predictions(self.pred_w if ship_type == 'w' else self.pred_c if ship_type == 'c' else self.pred_s, position)
+
             self.display_predictions()
+
     
-    # アスキーアートで表示する
+    # 確率分布をアスキーアートで表示する
     def display_predictions(self):
         print("Prediction for 'w':")
         self.print_ascii_art(self.pred_w)
@@ -198,7 +186,7 @@ class AIPlayer(Player):
         for i in range(Player.FIELD_SIZE):
             print(f" {i} |", end="")
             for j in range(Player.FIELD_SIZE):
-                print(f" {pred[i][j]:.2f} |", end="")
+                print(f" {pred[j][i]:.2f} |", end="")
             print("\n" + "--------" * Player.FIELD_SIZE)
             
     # 移動を反映させる
@@ -210,6 +198,58 @@ class AIPlayer(Player):
                 if Player.in_field([ni, nj]):
                     new_pred[ni][nj] = pred[i][j]
         return new_pred
+    
+    # 周囲のマスの数に基づいて，加算する値を決める．
+    # nearの場合，中心の1マスは計算しない．
+    def update_near_predictions(self, pred, position):
+        neighbors = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if (dx != 0 or dy != 0)]
+        valid_neighbors = [n for n in neighbors if Player.in_field([position[0] + n[0], position[1] + n[1]])]
+        factor = 1 / len(valid_neighbors)
+        for dx, dy in valid_neighbors:
+            x, y = position[0] + dx, position[1] + dy
+            pred[x][y] += factor
+        pred[position[0]][position[1]] = 0
+        self.divide_two(pred)
+
+    # 攻撃の場合，中心も含めて計算する．
+    def update_around_predictions(self, pred, position):
+        neighbors = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1]]
+        valid_neighbors = [n for n in neighbors if Player.in_field([position[0] + n[0], position[1] + n[1]])]
+        factor = 1 / len(valid_neighbors)
+        for dx, dy in valid_neighbors:
+            x, y = position[0] + dx, position[1] + dy
+            pred[x][y] += factor
+        self.divide_two(pred)
+        
+    # 周囲9マスを0にする．
+    def clear_around_predictions(self, pred, position):
+        # 値が1のマスがあれば更新を行わない．
+        if any(pred[i][j] == 1 for i in range(Player.FIELD_SIZE) for j in range(Player.FIELD_SIZE)):
+            return
+        neighbors = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1]]
+        valid_neighbors = [n for n in neighbors if Player.in_field([position[0] + n[0], position[1] + n[1]])]
+        for dx, dy in valid_neighbors:
+            x, y = position[0] + dx, position[1] + dy
+            pred[x][y] = 0
+                
+    # 確率分布を2で割るメソッド
+    def divide_two(self, pred):
+        # 初回は割る必要がないため，合計が約1であり，約2ではない場合は割らないようにする．
+        total = sum(sum(row) for row in pred)
+        # 1に近い値の場合は割らない．つまり，1との差が0.1よりも大きいならば割る．
+        if abs(total - 1) > 0.1:
+            for i in range(Player.FIELD_SIZE):
+                for j in range(Player.FIELD_SIZE):
+                    if pred[i][j] != 0:
+                        pred[i][j] /= 2
+
+    # 正規化を行う．ヒットしなかった場合に使用する．
+    def normalize(self, pred):
+        total = sum(sum(row) for row in pred)
+        if total > 0:
+            for i in range(Player.FIELD_SIZE):
+                for j in range(Player.FIELD_SIZE):
+                    pred[i][j] /= total
 
 
 # 仕様に従ってサーバとソケット通信を行う．
